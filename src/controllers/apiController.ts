288fbfe262124b15ef6ab3f63dc345b2cd50db28
rohit24-code <rohitsingh17031995@gmail.com>
2024-12-14 07:21:22 +0530

    fixed some issues

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,11 +1,11 @@
-import { Request,Response } from "express"
+import { Request,RequestHandler,Response } from "express"
 
 
-   const  self: RequestHandler :(_:Request,res:Response)=>{
+   const  self: RequestHandler =(_:Request,res:Response)=>{
       try {
-       return res.sendStatus(200)
+        res.sendStatus(200)
       } catch (error) {
-        return res.sendStatus(500)
+         res.sendStatus(500)
       }
     }
 
