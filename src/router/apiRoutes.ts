f5e75ffa0ea3fb8da001485992a7b169399d6dc8
rohit24-code <rohitsingh17031995@gmail.com>
2024-12-14 07:46:58 +0530

    feat: Added Prettier configuration

--- src/router/apiRoutes.ts
+++ src/router/apiRoutes.ts
@@ -1,8 +1,8 @@
-import { Router } from "express";
-import apiController from "../controllers/apiController";
+import { Router } from 'express'
+import apiController from '../controllers/apiController'
 
-const router=Router();
+const router = Router()
 
-router.route("/self").get(apiController.self)
+router.route('/self').get(apiController.self)
 
 export default router
